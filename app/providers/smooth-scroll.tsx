1d9dec917b3a86a25f06668f2d17ce82a7098a70
marisankarganesan <marisankar.ganesan@ethereal.io>
2026-02-06 22:57:49 +0530

    firstcom

---
+++ app/providers/smooth-scroll.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+
+export default function LenisProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.7,
+      smoothWheel: true,
+    //   lerp: 0.07, // 🧈 butter smooth
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
